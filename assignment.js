772a1473c477b2a004095af82407f9584a6ed46a
trishan <trishansaha43@gmail.com>
2021-01-22 08:22:05 -0800

    this is my third assignment

---
+++ assignment.js
@@ -0,0 +1,53 @@
+
+// Kilometer To Meter Converter Here
+
+// function kilometerToMeter(kiloMeter) {
+//     var result =   kiloMeter * 1000
+//     return result
+// }
+// var result = kilometerToMeter(1.5)
+// console.log(result)
+
+
+// // Budget Calculator
+// function budgetCalculator(watch, phone, laptop ) {
+//     var singleWatch = 50
+//     var singlePhone = 100
+//     var singlelaptop = 500
+
+//     var multiWatch = watch * singleWatch
+//     var multiPhone =  phone * singlePhone
+//     var multiLaptop = laptop * singlelaptop
+
+//     return multiWatch + multiPhone + multiLaptop
+// }
+// var budgetCal = budgetCalculator(2, 1, 1)
+// console.log(budgetCal)
+
+
+// hotel cost
+function hotelCost(days) {
+    var firstPrice = 100
+    var secondPrice = 80
+    var lastPrice = 50
+    if (days <= 10 && days <= 20) {
+        var firstTotal = days * firstPrice 
+        var secondTotal = days * secondPrice
+        var finalTotal = firstTotal + secondTotal
+        return finalTotal
+        
+    } 
+}
+var outputHotel = hotelCost(11)
+console.log(outputHotel)
+
+
+// megaFriend
+
+
+
+
+
+
+
+
